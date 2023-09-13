ac7aa0c4c3ba1c5779b38ecdc170b870297ef5a3
Holberton1990 <ersida9019@gmail.com>
2023-09-13 05:27:30 -0700

    sorty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "sort.h"
 /*
- *
- *
- *
+  * @swap: swap
+  * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
  */
 void swap(int *xp, int *yp)
 {
@@ -15,10 +16,16 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {	
 	size_t i, j;
-	for (i = 0; i < size - 1; i++) {
-	for (j = 0; j size - i - 1; j++) {
-		if(array[j] > &array[j + 1]) {
-		swap(&array[j], &array[j + 1]);
-		print_array(array, size);
+	for (i = 0; i < size - 1; i++)
+	{
+		
+		for (j = 0; j size - i - 1; j++)
+	{
+		if(array[j] > array[j + 1])
+		{
+			swap(&array[j], &array[j + 1]);
+			print_array(array, size);
 		}	
-
+	}
+	}
+}
