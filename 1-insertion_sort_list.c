b6969793a3df03c33fec86bf4e490636193c8e76
Holberton1990 <ersida9019@gmail.com>
2023-09-14 05:29:50 -0700

    task 0, 1, 2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,30 +8,27 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *sorted, *next;
 
-		if (list == NULL || *list == NULL)	
-			return;	
-		current = (*list)->next;	
-		
-		while (current != NULL)
-{	
-	sorted = current;
-        next = sorted->next;
-	
+	if (list == NULL || *list == NULL)
+		return;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		sorted = current;
+		next = sorted->next;
 	while (sorted->prev != NULL && sorted->n < sorted->prev->n)
-{
-	if (sorted->next != NULL)
-		sorted->next->prev = sorted->prev;
-	sorted->prev->next = sorted->next;
-	sorted->next = sorted->prev;
-	sorted->prev = sorted->prev->prev;
-	sorted->next->prev = sorted;
-	if (sorted->prev != NULL)
-		sorted->prev->next = sorted;
-	else
-		*list = sorted;
-	print_list(*list);
-}
-current = next;
-}
-
+	{
+		if (sorted->next != NULL)
+			sorted->next->prev = sorted->prev;
+		sorted->prev->next = sorted->next;
+		sorted->next = sorted->prev;
+		sorted->prev = sorted->prev->prev;
+		sorted->next->prev = sorted;
+		if (sorted->prev != NULL)
+			sorted->prev->next = sorted;
+		else
+			*list = sorted;
+		print_list(*list);
+	}
+	current = next;
+	}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,32 +1,29 @@
 #include "sort.h"
-
 /**
  * selection_sort - Sorts an array of integers in ascending order
- *                  using the Selection sort algorithm.
+ * using the Selection sort algorithm.
  * @array: The array of integers to be sorted.
  * @size: The number of elements in the array.
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int tmp;
+	size_t i, j, min;
+	int tmp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[min] > array[j])
-                min = j;
-        }
-
-        if (min != i)
-        {
-            tmp = array[i];
-            array[i] = array[min];
-            array[min] = tmp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
 }
-
