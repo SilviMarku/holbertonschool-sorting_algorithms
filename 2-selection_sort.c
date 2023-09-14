832360a584cb2397005e6c73634212a7c4d31936
Holberton1990 <ersida9019@gmail.com>
2023-09-14 04:59:35 -0700

    sorty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,9 @@
 void selection_sort(int *array, size_t size)
 {	
 	int i, j, min;
-	for(i = 0; i < (n - 1); i++)
+	int tmp;
+
+	for(i = 0; i < (size - 1); i++)
 {	
 	min = i;
 	for(j = i + 1; j < size; j++)
