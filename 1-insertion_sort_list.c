7ff55109a8d4543905016b95b53629e45dbc48a7
Holberton1990 <ersida9019@gmail.com>
2023-09-14 03:21:16 -0700

    sorty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,3 +1,9 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: A pointer to the pointer to the head of the list.
+ */
 void insertion_sort_list(listint_t **list)
 {
     listint_t *current, *sorted, *next;
