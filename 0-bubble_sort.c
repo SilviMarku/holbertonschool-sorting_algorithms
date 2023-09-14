967be501c07ecf36205fc52850440d4896083771
Holberton1990 <ersida9019@gmail.com>
2023-09-14 01:58:52 -0700

    sorty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "sort.h"
-
 /**
  * bubble_sort - Sorts an array of integers.
  * @array: The array to be sorted.
@@ -8,30 +7,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+    size_t i, j;
 
-	if (array == NULL || size == 0)
-		return;
+    if (array == NULL || size == 0)
+        return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		int swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				int tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				swapped = 1;
+    for (i = 0; i < size - 1; i++)
+    {
+        int swapped = 0;
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                swapped = 1;
 
-				print_array(array, size);
-			}
-		}
-		if (swapped == 0)
-		{
-			break;
-		}
-	}
+                print_array(array, size);
+            }
+        }
+        if (swapped == 0)
+        {
+            break;
+        }
+    }
 }
 
