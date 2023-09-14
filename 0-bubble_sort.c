51842b59930932e0ed49fde080bfe77671052bd9
Holberton1990 <ersida9019@gmail.com>
2023-09-14 01:30:38 -0700

    sorty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,8 +8,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+    size_t i, j,tmp;
 
     for (i = 0; i < size - 1; i++)
     {
@@ -18,9 +17,9 @@ void bubble_sort(int *array, size_t size)
         {
             if (array[j] > array[j + 1])
             {
-                int temp = array[j];
+                int tmp = array[j];
                 array[j] = array[j + 1];
-                array[j + 1] = temp;
+                array[j + 1] = tmp;
                 Swap = 1;
 
                 print_array(array, size); 
