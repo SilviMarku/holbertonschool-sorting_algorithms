31dc2300afa6885133617802438cf1b3c74f451b
Holberton1990 <ersida9019@gmail.com>
2023-09-14 03:44:46 -0700

    sorty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,16 +7,13 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *sorted, *next;
-	
 	if (list == NULL || *list == NULL)
         return;
-
-	current = (*list)->next;
+	current = (*list)->next;	
 	while (current != NULL)
 {
 	sorted = current;
         next = sorted->next;
-
 	while (sorted->prev != NULL && sorted->n < sorted->prev->n)
 {
 	if (sorted->next != NULL)
