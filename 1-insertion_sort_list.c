175f8664bc26abcde32991ea50bb004ed44cbfa0
Holberton1990 <ersida9019@gmail.com>
2023-09-14 03:49:39 -0700

    sorty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  * in ascending order using the Insertion sort algorithm.
@@ -6,30 +7,36 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *sorted, *next;
-	if (list == NULL || *list == NULL)
-        return;
-	current = (*list)->next;	
-	while (current != NULL)
-{
-	sorted = current;
-        next = sorted->next;
-	while (sorted->prev != NULL && sorted->n < sorted->prev->n)
-{
-	if (sorted->next != NULL)
-		sorted->next->prev = sorted->prev;
-	sorted->prev->next = sorted->next;
-	sorted->next = sorted->prev;
-	sorted->prev = sorted->prev->prev;
-	sorted->next->prev = sorted;
-	if (sorted->prev != NULL)
-		sorted->prev->next = sorted;
-	else
-		*list = sorted;
-	print_list(*list);
-}
-current = next;
-}
+    listint_t *current, *sorted, *next;
 
+    if (list == NULL || *list == NULL)
+        return;
+
+    current = (*list)->next;
+
+    while (current != NULL)
+    {
+        sorted = current;
+        next = sorted->next;
+
+        while (sorted->prev != NULL && sorted->n < sorted->prev->n)
+        {
+            if (sorted->next != NULL)
+                sorted->next->prev = sorted->prev;
+            sorted->prev->next = sorted->next;
+            sorted->next = sorted->prev;
+            sorted->prev = sorted->prev->prev;
+            sorted->next->prev = sorted;
+
+            if (sorted->prev != NULL)
+                sorted->prev->next = sorted;
+            else
+                *list = sorted;
+
+            print_list(*list);
+        }
+
+        current = next;
+    }
 }
 
