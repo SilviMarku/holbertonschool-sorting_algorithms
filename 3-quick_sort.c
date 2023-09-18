0bae5e1659b0b97cb0b99c6ad0ee98aaa9202d61
Holberton1990 <ersida9019@gmail.com>
2023-09-18 01:13:04 -0700

    sorty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to swap two elements
+
 void swap(int* a, int* b)
 {
     int t = *a;
@@ -8,29 +8,28 @@ void swap(int* a, int* b)
     *b = t;
 }
 
-// Partition the array using the last element as the pivot
+
 int partition(int arr[], int low, int high)
 {
-    // Choosing the pivot
+   
     int pivot = arr[high];
 
-    // Index of smaller element and indicates
-    // the right position of pivot found so far
+  
     int i = (low - 1);
 
     int j;
     for (j = low; j <= high - 1; j++) {
 
-        // If current element is smaller than the pivot
+        
         if (arr[j] < pivot) {
 
-            // Increment index of smaller element
+            
             i++;
             
-            // Swap arr[i] and arr[j]
+          
             swap(&arr[i], &arr[j]);
             
-            // Print the array after each swap
+            
             printf("Swapped %d and %d: ", arr[i], arr[j]);
             for (int k = low; k <= high; k++) {
                 printf("%d ", arr[k]);
